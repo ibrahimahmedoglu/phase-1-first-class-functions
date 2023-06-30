88e2fa5ff2ea9d69e29a1d352711246aa08b604e
ibrahimahmedoglu <ibrahim.ahmedoglu@bahcesehir.edu.tr>
2023-06-30 14:11:12 +0300

    completed assigntment

---
+++ index.js
@@ -0,0 +1,18 @@
+// index.js
+
+function receivesAFunction(callback) {
+    callback();
+  }
+  
+  function returnsANamedFunction() {
+    return function namedFunction() {
+      console.log("This is a named function.");
+    };
+  }
+  
+  function returnsAnAnonymousFunction() {
+    return function () {
+      console.log("This is an anonymous function.");
+    };
+  }
+  
